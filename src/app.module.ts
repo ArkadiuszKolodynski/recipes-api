5dc6e734855ef5bb8ef3d270743ce1d0884ac94f
ArkadiuszKolodynski <ArkadiuszKolodynski@users.noreply.github.com>
2021-08-14 23:21:44 +0200

    Add users resource (#3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { RecipesModule } from './recipes/recipes.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, RecipesModule],
+  imports: [PrismaModule, RecipesModule, UsersModule],
 })
 export class AppModule {}
